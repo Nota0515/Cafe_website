53e51934301c5b31c9254d549f7d74dbb5acd2b2
Nota0515 <businessbro78@gmail.com>
2024-11-30 20:20:13 +0530

    made card Component & implement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,14 @@
 
 const Card = ({src , className , name , price , alt}) => {
   return (
-    <div className="flex w-72 h-[25rem] md:w-72 md:h-[25rem] lg:w-[21.25rem] lg:h-[26.25rem] border-[1px] border-n-3 rounded-md">
-      <div className="w-full h-[20rem] md:h-[20rem] lg:h-[21rem]  bg-red-400 rounded-t-[5px] overflow-hidden"></div>
-      <div></div>
+    <div className={`relative w-72 h-[25rem] md:w-72 md:h-[25rem] lg:w-[21.25rem] lg:h-[26.25rem] border-[1px] border-n-7 rounded-md overflow-hidden ${className || ''}`}>
+      <div className="w-full h-[20rem] md:h-[20rem] lg:h-[21rem] rounded-t-[5px] shadow-inner-darkest overflow-hidden">
+        <img src={src} className="w-full h-full object-cover object-center " alt={alt}/>
+      </div>
+      <div className="absolute bottom-0 w-full h-[6rem] py-6 px-2">
+        <span className="text-n-3 text-xl font-montserrat font-thin">{name}</span><br></br>
+        <span className="text-n-3 text-2xl font-montserrat font-normal" >Price: ₹{price}</span>
+      </div>
     </div>
   )
 }
--- src/sections/Speciality.jsx
+++ src/sections/Speciality.jsx
@@ -1,12 +1,13 @@
 import Card from "../components/Card"
+import { hero4 , hero1 , hero3, jinidosa, oniondosa, maisoredosa, pannerdosa, noodledosa } from "../assets"
 
 const Speciality = () => {
   return (
     <div className="flex flex-col pl-10 mb-10 md:px-20 gap-y-10 md:flex-row lg:flex-row justify-evenly w-full gap-x-10 flex-wrap">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card name={"Edli Vada"} alt={"idli-vada-img"} price={"40(4piece)"} src={hero3}/>
+        <Card name={"Onion Dosa"} alt={"Dosa-img"} price={"60/-"} src={hero4}/>
+        <Card name={"Edli"} alt={"idli-img"} price={"40(4piece)"} src={hero1}/>
+        <Card name={"Jini Dosa"} alt={"idli-img"} price={"60/-"} src={jinidosa}/>
     </div>
   )
 }
