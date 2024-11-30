702654104c24ee2dde821bb85ea9617450f45e7e
Nota0515 <businessbro78@gmail.com>
2024-11-30 17:14:47 +0530

    add margin bottom

--- src/sections/Speciality.jsx
+++ src/sections/Speciality.jsx
@@ -2,7 +2,7 @@ import Card from "../components/Card"
 
 const Speciality = () => {
   return (
-    <div className="flex flex-col pl-10 md:px-20 gap-y-10 md:flex-row lg:flex-row justify-evenly w-full gap-x-10 flex-wrap">
+    <div className="flex flex-col pl-10 mb-10 md:px-20 gap-y-10 md:flex-row lg:flex-row justify-evenly w-full gap-x-10 flex-wrap">
         <Card />
         <Card />
         <Card />
