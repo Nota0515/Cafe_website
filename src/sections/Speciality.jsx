53b3fdc030a34411bacfcf22faaac26ca45caa90
Nota0515 <businessbro78@gmail.com>
2024-11-30 20:31:46 +0530

    added bottom border

--- src/sections/Speciality.jsx
+++ src/sections/Speciality.jsx
@@ -3,7 +3,7 @@ import { hero4 , hero1 , hero3, jinidosa, oniondosa, maisoredosa, pannerdosa, no
 
 const Speciality = () => {
   return (
-    <div className="flex flex-col pl-10 mb-10 md:px-20 gap-y-10 md:flex-row lg:flex-row justify-evenly w-full gap-x-10 flex-wrap">
+    <div className="flex flex-col pl-10 mb-10 md:px-20 gap-y-10 md:flex-row lg:flex-row justify-evenly border-b-[1px] pb-16 border-b-n-7 w-full gap-x-10 flex-wrap">
         <Card name={"Edli Vada"} alt={"idli-vada-img"} price={"40(4piece)"} src={hero3}/>
         <Card name={"Onion Dosa"} alt={"Dosa-img"} price={"60/-"} src={hero4}/>
         <Card name={"Edli"} alt={"idli-img"} price={"40(4piece)"} src={hero1}/>
