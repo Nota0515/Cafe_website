8a77948809e07f7e2eabe593f1a76d9afbc34a4f
Nota0515 <businessbro78@gmail.com>
2024-12-03 19:24:22 +0530

    made ratings component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,23 @@
-import { FaStar } from "react-icons/fa";
+import { FaStar , FaStarHalf } from "react-icons/fa";
 
 const Rating = ({num}) => {
+  const rating = [];
+  if(num==4.5){
+    for (let i=0; i<4; i++){
+      rating.push(<FaStar key={i} color="#DDBE63"/>);
+    }
+    rating.push(<FaStarHalf color="#DDBE63" />);
+  }else{
+    for (let i=0 ; i<num; i++){
+      rating.push(<FaStar key={i} color="#DDBE63" />);
+    }
+  }
+
   return (
     <div className="flex items-center">
-        <FaStar color="#DDBE63"/>
+      {rating}
     </div>
   )
 }
 
-export default Rating
+export default Rating;
