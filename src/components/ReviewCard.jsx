481ecd88cf9eb38654e82426eb086873cc09658e
Nota0515 <businessbro78@gmail.com>
2024-12-03 22:17:41 +0530

    created ReviewCard component

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,11 +1,11 @@
 import Rating from "./Rating"
 
-const ReviewCard = ({src , name}) => {
+const ReviewCard = ({src , name , discrpt , num }) => {
   return (
-    <div className="flex-col bg-n-5 items-center rounded-lg w-[16rem] h-[7.563rem] shadow-lg flex-nowrap px-4 py-4 ">
+    <div className="flex-col bg-n-5 items-center rounded-lg gap-y-4 w-[16rem] h-[7.563rem] shadow-lg flex-nowrap px-4 py-4 ">
         <div className="flex flex-row w-[13rem] justify-between">
             <div className="flex flex-row gap-3 items-center flex-nowrap">
-                <div className="w-8 h-8 rounded-full bg-n-5 overflow-hidden" >
+                <div className="w-8 h-8 shrink-0 rounded-full bg-n-5 overflow-hidden" >
                     <img src={src} alt="customerImg" className="object-cover object-center" />
                 </div>
                 <div className="inline-block">
@@ -13,8 +13,11 @@ const ReviewCard = ({src , name}) => {
                 </div>
             </div>
             <div className="flex flex-row">
-                <Rating />
+                <Rating num={num} />
             </div>
+        </div>
+        <div className="flex mt-1 flex-wrap leading-3 w-[13rem] h-auto">
+            <span className=" text-xs font-poppins text-n-1 text-left">{discrpt}</span>
         </div>
     </div>
   )
